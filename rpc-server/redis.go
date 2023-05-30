c749d1c3cf3117a618299f681240d1e5f8b71f97
Nyx <tessasie15@gmail.com>
2023-05-30 15:25:27 +0800

    Added a Message struct (Message.go) and a saveMessage function in redis.go.

--- rpc-server/redis.go
+++ rpc-server/redis.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"encoding/json"
 	"github.com/redis/go-redis/v9"
 )
 
@@ -22,5 +23,25 @@ func (c *RedisClient) InitClient(ctx context.Context, address, password string) 
 	}
 
 	c.cli = r
+	return nil
+}
+
+func (c *RedisClient) SaveMessage(ctx context.Context, roomID string, message *Message) error {
+	// Marshal the Go struct into JSON bytes
+	text, err := json.Marshal(message)
+	if err != nil {
+		return err
+	}
+
+	member := &redis.Z{
+		Score:  float64(message.Timestamp), // The sort key
+		Member: text,                       // Data
+	}
+
+	_, err = c.cli.ZAdd(ctx, roomID, *member).Result()
+	if err != nil {
+		return err
+	}
+
 	return nil
 }
