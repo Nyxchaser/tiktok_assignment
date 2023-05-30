7b8d01a0a22584dbc40372638d14b64a7e26e0fa
Nyx <tessasie15@gmail.com>
2023-05-30 15:27:57 +0800

    Added a GetMessagesByRoomID function in redis.go to get messages from Redis.

--- rpc-server/redis.go
+++ rpc-server/redis.go
@@ -45,3 +45,36 @@ func (c *RedisClient) SaveMessage(ctx context.Context, roomID string, message *M
 
 	return nil
 }
+
+func (c *RedisClient) GetMessagesByRoomID(ctx context.Context, roomID string, start, end int64, reverse bool) ([]*Message, error) {
+	var (
+		rawMessages []string
+		messages    []*Message
+		err         error
+	)
+
+	if reverse {
+		// Desc order with time -> first message is the latest message
+		rawMessages, err = c.cli.ZRevRange(ctx, roomID, start, end).Result()
+		if err != nil {
+			return nil, err
+		}
+	} else {
+		// Asc order with time -> first message is the earliest message
+		rawMessages, err = c.cli.ZRange(ctx, roomID, start, end).Result()
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	for _, msg := range rawMessages {
+		temp := &Message{}
+		err := json.Unmarshal([]byte(msg), temp)
+		if err != nil {
+			return nil, err
+		}
+		messages = append(messages, temp)
+	}
+
+	return messages, nil
+}
